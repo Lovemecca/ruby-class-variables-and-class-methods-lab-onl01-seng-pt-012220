bd7957834a84257d36cb00d5faca1e69eba8c8ea
Mecca Jay <mecca.nicolej@gmail.com>
2020-02-18 02:09:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,9 +26,9 @@ class Song
 def initialize (name, artist, genre)
   @name = name
   @artist = artist 
-  @@artist << artist 
+  @@artists << artist 
   @genre = genre 
-  @@genre << genre 
+  @@genres << genre 
   @@count += 1 
 end 
   
